ed5dabed85faa2bbb56b2c0180d267c688b4ffdd
jbuican14 <jbuican19@gmail.com>
2025-01-03 19:25:53 +0000

    auth package

--- src/token_frontend/src/main.jsx
+++ src/token_frontend/src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { AuthClient } from '@dfinity/auth-client';
 import './index.scss';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const init = async () => {
+  const authClient = await AuthClient.create();
+
+  if (await authClient.isAuthenticated()) {
+    handleAuthenticated(authClient);
+  } else {
+    await authClient.login({
+      identityProvider: 'https://identity.ic0.app/#authrize',
+      onSuccess: () => {
+        handleAuthenticated(authClient);
+      },
+    });
+  }
+};
+
+async function handleAuthenticated(authClient) {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+init();
